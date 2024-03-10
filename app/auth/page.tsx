a6b8a637483bfa16fa9c63e4fa390680338b0d48
Amit Biswas <amit.142biswas@gmail.com>
2024-03-10 13:25:08 +0530

    not working

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='relative h-full w-full bg-[url("/images/hero.jpg")]'>
+      Auth
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="bg-black text-red-600">Netflix</h1>
+    <h1 className="text-red-600">Netflix</h1>
     </>
   );
 }
